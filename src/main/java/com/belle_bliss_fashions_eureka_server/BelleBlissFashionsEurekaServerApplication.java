1526934a5d62f79c3817333290b5739a77f1fc20
Kasun Fernando <fernandokasun672@gmail.com>
2024-08-30 13:36:17 +0530

    init

---
+++ src/main/java/com/belle_bliss_fashions_eureka_server/BelleBlissFashionsEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.belle_bliss_fashions_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BelleBlissFashionsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelleBlissFashionsEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belle_bliss_fashions_eureka_server/BelleBlissFashionsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belle_bliss_fashions_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelleBlissFashionsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
